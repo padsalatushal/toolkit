79dd0414ab1ae2feae0b4ff040aad2561199a24e
Padsala Tushal <padsalatushal@gmail.com>
2023-06-20 14:34:07 +0530

    Update tool.ps1

--- tool.ps1
+++ tool.ps1
@@ -1,11 +1,25 @@
+# Check for admin 
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Output "Needs to be ran as Administrator. Attempting to relaunch."
+        Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -Command `"iwr -useb https://raw.githubusercontent.com/padsalatushal/toolkit/main/tool.ps1 | iex`""
+    
+        break
+    }
+# check for execution policy 
+$executionPolicy = Get-ExecutionPolicy
+if ($executionPolicy -ne "Bypass") {
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -Command `"iwr -useb https://raw.githubusercontent.com/padsalatushal/toolkit/main/tool.ps1 | iex`""
+    break     
+}
+
 Add-Type -AssemblyName PresentationFramework
 [xml]$xaml = @"
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         x:Name="Window"
         Title="Window"
-        Width="400"
-        Height="300">
+        Width="600"
+        Height="350">
 
     <Grid x:Name="Grid" Margin="20">
         <Grid.RowDefinitions>
@@ -29,8 +43,8 @@ Add-Type -AssemblyName PresentationFramework
                 Margin="5"
         />
         
-        <Button x:Name="WPFRemoveEdgeSoft"
-                Content="Remove Microsoft Edge(Temparary)"
+        <Button x:Name="WPFburpproinstaller"
+                Content="Install Burp Suite Pro"
                 Grid.Column="1"
                 Grid.Row="0"
                 Width="200"
@@ -47,8 +61,8 @@ Add-Type -AssemblyName PresentationFramework
                 Margin="5"
         />
 
-        <Button x:Name="Button4"
-                Content="Activate Windows + Office"
+        <Button x:Name="WPFwindowsactivatortoolkit"
+                Content="Windows Activate Toolkit "
                 Grid.Column="0"
                 Grid.Row="1"
                 Width="100"
@@ -69,16 +83,38 @@ Add-Type -AssemblyName PresentationFramework
 </Window>
 
 "@
+
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 $window = [Windows.Markup.XamlReader]::Load($reader)
 
 
 $WPFwinutil = $window.FindName("WPFwinutil")
 $WPFwinutil.Add_Click({
-    irm https://christitus.com/win | iex
+        Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -Command `"iwr -useb https://christitus.com/win | iex`""
 })
+
 $WPFRemoveEdgeHard = $window.FindName("WPFRemoveEdgeHard")
 $WPFRemoveEdgeHard.Add_Click({
-        irm https://padsalatushal.github.io/edge.bat | iex
+        Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -Command `"iwr -useb https://padsalatushal.github.io/edge.bat | iex`""
+
 })
+
+
+
+$WPFburpproinstaller = $window.FindName("WPFburpproinstaller")
+$WPFburpproinstaller.Add_Click({
+        Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -Command `"iwr -useb https://padsalatushal.github.io/burp_setup.ps1 | iex`""
+
+})
+
+
+$WPFwindowsactivatortoolkit = $window.FindName("WPFwindowsactivatortoolkit")
+$WPFwindowsactivatortoolkit.Add_Click({
+        Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -Command `"iwr -useb https://massgrave.dev/get | iex`""
+
+})
+
+
+
+
 $window.ShowDialog()
